63881aaa83e3cfd96bc86e2b168d65dd21f2e942
BodonFerenc <bodonferenc@gmail.com>
2019-09-09 16:49:15 +0200

    minor improvement in command line  handling

--- xlsToGedcom.py
+++ xlsToGedcom.py
@@ -5,9 +5,13 @@ from tableToGedcom import FamilyTreeMapping
 import pyexcel as pe
 
 def main(argv):
-    t= pe.get_records(file_name=sys.argv[1])
-    ft = FamilyTreeMapping(t, sys.argv[2])
-    ft.printGedcom(sys.argv[3], sys.argv[4])
+    # TODO consider using getopt
+    if len(argv) < 4: 
+        print("Four parameters are mandatory: input file, picture directory, first name and last name", file=sys.stderr)
+        return
+    t = pe.get_records(file_name=argv[0])
+    ft = FamilyTreeMapping(t, argv[1])
+    ft.printGedcom(argv[2], argv[3])
 
 if __name__ == "__main__":
-   main(sys.argv[1:])    
+   main(sys.argv[1:])
